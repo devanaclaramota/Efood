5589b797c63f7ac26da7e77f7e40a5d0dc8040f5
Ana clara <dev.anaclaramota@outlook.com>
2023-08-06 17:58:16 -0300

    new-commit

--- src/components/Tag/index.jsx
+++
@@ -1,5 +0,0 @@
-import { Tg } from "./styles"
-export const Tag = ({txt}) => {
-
-    return <Tg>{txt}</Tg>
-}
--- src/components/Tag/styles.jsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-export const Tg = styled.div`
-    background-color: #e66767;
-    color: white;
-    text-align: center;
-    padding: 4px 8px;
-    cursor: pointer;
-  
-`
